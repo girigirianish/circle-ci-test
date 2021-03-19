a1555cbb5b775dc5a5d2e8129ca279e80c78e685
girigirianish <giri.girianish@gmail.com>
2021-03-19 18:30:16 +0545

    Update qualiti-script.sh

--- qualiti-script.sh
+++ qualiti-script.sh
@@ -11,23 +11,25 @@ API_URL='https://7iggpnqgq9.execute-api.us-east-2.amazonaws.com/udbodh/api'
 INTEGRATION_JWT_TOKEN='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0X2lkIjozMzIsImFwaV9rZXlfaWQiOjI0ODMsIm5hbWUiOiIiLCJkZXNjcmlwdGlvbiI6IiIsImljb24iOiIiLCJpbnRlZ3JhdGlvbl9uYW1lIjoiR2l0bGFiIiwib3B0aW9ucyI6e30sImlhdCI6MTYxNjE1NjI2OX0.F4C59OAbjnhoA9OK_5MRoDlbL-4AVNiu3rD0S6Khj7c'
 INTEGRATIONS_API_URL='http://52708b9d8a8b.ngrok.io'
  
+​
 apt-get update -y
 apt-get install -y jq
- 
+​
 #Trigger test run
 TEST_RUN_ID="$( \
-  curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/gitlab/${PROJECT_ID}/events \
+  curl -X POST -G ${INTEGRATIONS_API_URL}/api/integrations/azure/${PROJECT_ID}/events \
     -d 'token='$INTEGRATION_JWT_TOKEN''\
     -d 'triggerType=Deploy'\
+    -d 'projectId='$PROJECT_ID''\
   | jq -r '.test_run_id')"
- 
+​
 AUTHORIZATION_TOKEN="$( \
   curl -X POST -G ${API_URL}/auth/token \
   -H 'x-api-key: '${API_KEY}'' \
   -H 'client_id: '${CLIENT_ID}'' \
   -H 'scopes: '${SCOPES}'' \
   | jq -r '.token')"
- 
+​
 # Wait until the test run has finished
 while : ; do
    RESULT="$( \
@@ -35,12 +37,15 @@ while : ; do
    -H 'token: Bearer '$AUTHORIZATION_TOKEN'' \
    -H 'x-api-key: '${API_KEY}'' \
   | jq -r '.[0].finished')"
+  # echo $RESULT
   if [ "$RESULT" != null ]; then
+    echo "the result is not null : $RESULT"
     break;
   fi
+    echo "the result is : $RESULT"
     sleep 15;
 done
- 
+​
 # # Once finished, verify the test result is created and that its passed
 TEST_RUN_RESULT="$( \
   curl -X GET ${API_URL}/test-results?test_run_id=${TEST_RUN_ID}\&project_id=${PROJECT_ID} \
@@ -48,4 +53,4 @@ TEST_RUN_RESULT="$( \
     -H 'x-api-key: '${API_KEY}'' \
   | jq -r '.[0].status' \
 )"
-echo "Qualiti E2E Tests ${TEST_RUN_RESULT}"
+echo "Qualiti E2E Tests ${TEST_RUN_RESULT
